91fdd9dada865eb6ca85a8ef6f4a8a4acfdaefb7
abdeokar23 <abdeokar23@gmail.com>
2017-12-08 20:24:54 +0530

    Implemented SSL

--- tcpClient.py
+++ tcpClient.py
@@ -1,6 +1,7 @@
 import socket
 import hashlib
 from Crypto.Cipher import AES
+import ssl
 
 KEY = hashlib.sha256("some random password").digest()
 
@@ -9,23 +10,25 @@ obj = AES.new(KEY, AES.MODE_CFB, IV)
 
 def main():
 	host = "127.0.0.1"
-	port = 5000
+	port = 5001
 
-	s=socket.socket()
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	ssl_sock = ssl.wrap_socket(s, cert_reqs=ssl.CERT_REQUIRED, ca_certs='/home/maverick/Practice/Socket_Prog/key_cert/sslCert.crt')
 
-	s.connect((host,port))
+	ssl_sock.connect((host,port))
 
 	message = raw_input("-> ")
 	while message != 'q':
-		s.send(message)
-		data = s.recv(1024)
+		#s.send(message)
+		ssl_sock.write(message)
+		data = ssl_sock.recv(1024)
 		print "received data: "+str(data)
 		print "decrypting..."
 		decrypted = obj.decrypt(data)
 		print "received from server "+str(decrypted)
 		message = raw_input("-> ")
 
-	s.close()
+	ssl_sock.close()
 
 if __name__ == "__main__":
 	main()
--- tcpServer.py
+++ tcpServer.py
@@ -1,26 +1,21 @@
 import socket
+from socket import AF_INET, SOCK_STREAM, SO_REUSEADDR, SOL_SOCKET, SHUT_RDWR
 import hashlib
 from Crypto.Cipher import AES
+import ssl
+from ssl import PROTOCOL_TLS
+
+keyFile = "/home/maverick/Practice/Socket_Prog/key_cert/privateKey.pem"
+certFile = "/home/maverick/Practice/Socket_Prog/key_cert/sslCert.crt"
 
 KEY = hashlib.sha256("some random password").digest()
 
 IV = "abcdefghijklmnop"
 obj = AES.new(KEY, AES.MODE_CFB, IV)
 
-def main():
-	host = "127.0.0.1"
-	port = 5000
-
-	s=socket.socket()
-	s.bind((host,port))
-
-	s.listen(1)
-
-	c, addr = s.accept()
-	print "connected with: "+str(addr)
-
+def echo_client(s):
 	while True:
-		data = c.recv(1024)
+		data = s.recv(1024)
 		if not data:
 			break
 		print "recieved from connection: "+str(data)
@@ -29,8 +24,27 @@ def main():
 		print "encrypting..."
 		print "encrypted data: "+str(encrypted)
 		print "sending: "+str(data)
-		c.send(encrypted)
-	c.close()
+		s.send(encrypted)
+	s.close()
+
+def main():
+	host = "127.0.0.1"
+	port = 5001
+
+	s=socket.socket(AF_INET, SOCK_STREAM)
+	s.bind((host,port))
+
+	s.listen(1)
+	s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+	
+	s_ssl=ssl.wrap_socket(s, keyfile=keyFile, certfile=certFile, server_side=True)
+	while True:
+		try:
+			c, addr = s_ssl.accept()
+			print "connected with: "+str(addr)
+			echo_client(c)
+		except socket.error as e:
+			print "Error:{0}".format(e)
 
 if __name__ == "__main__":
 	main()
