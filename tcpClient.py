de2544f71016eb4a7d42f0f7417a0de726d9a7b6
abdeokar23 <abdeokar23@gmail.com>
2017-12-09 12:08:09 +0530

    Comments added

--- tcpClient.py
+++ tcpClient.py
@@ -3,28 +3,29 @@ import hashlib
 from Crypto.Cipher import AES
 import ssl
 
-KEY = hashlib.sha256("some random password").digest()
+KEY = hashlib.sha256("some random password").digest()	#this will convert any pnemonic string which the user wants to choose as password to a 32 bit encrypted object
 
-IV = "abcdefghijklmnop"
-obj = AES.new(KEY, AES.MODE_CFB, IV)
+IV = "abcdefghijklmnop"		#Initialization vector should always be 16 bit
+obj = AES.new(KEY, AES.MODE_CFB, IV)	#creating an object to encrypt our data with
 
 def main():
 	host = "127.0.0.1"
 	port = 5001
 
-	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)	#specifying TCP and IPv4
 	ssl_sock = ssl.wrap_socket(s, cert_reqs=ssl.CERT_REQUIRED, ca_certs='sslCert.crt')	#provide full path to the certificate file location
+	#the above function "wraps" the socket created with the security layer of signed certificate
 
 	ssl_sock.connect((host,port))
 
 	message = raw_input("-> ")
 	while message != 'q':
-		#s.send(message)
-		ssl_sock.write(message)
+		#s.send(message)	is what we would have used in a socket program without ssl
+		ssl_sock.write(message)		#instead of the .send() we use .write() for ssl
 		data = ssl_sock.recv(1024)
 		print "received data: "+str(data)
 		print "decrypting..."
-		decrypted = obj.decrypt(data)
+		decrypted = obj.decrypt(data)	#using the object we created to decrypt the incoming data
 		print "received from server "+str(decrypted)
 		message = raw_input("-> ")
 
--- tcpServer.py
+++ tcpServer.py
@@ -8,10 +8,10 @@ from ssl import PROTOCOL_TLS
 keyFile = "privateKey.pem"	#provide full path to the private key file location
 certFile = "sslCert.crt"	#provide full path to the Certificate file location
 
-KEY = hashlib.sha256("some random password").digest()
+KEY = hashlib.sha256("some random password").digest()	#this will convert any pnemonic string which the user wants to choose as password to a 32 bit encrypted object
 
-IV = "abcdefghijklmnop"
-obj = AES.new(KEY, AES.MODE_CFB, IV)
+IV = "abcdefghijklmnop"		#Initialization vector should always be 16 bit
+obj = AES.new(KEY, AES.MODE_CFB, IV)	#creating an object to encrypt our data with
 
 def echo_client(s):
 	while True:
@@ -19,8 +19,8 @@ def echo_client(s):
 		if not data:
 			break
 		print "recieved from connection: "+str(data)
-		data = str(data).upper()
-		encrypted = obj.encrypt(data)
+		data = str(data).upper()	#converting the received string t upper case
+		encrypted = obj.encrypt(data)	#encrypting the data to be sent using the AES object we created
 		print "encrypting..."
 		print "encrypted data: "+str(encrypted)
 		print "sending: "+str(data)
@@ -31,13 +31,18 @@ def main():
 	host = "127.0.0.1"
 	port = 5001
 
-	s=socket.socket(AF_INET, SOCK_STREAM)
-	s.bind((host,port))
+	s=socket.socket(AF_INET, SOCK_STREAM)	#specifying TCP and IPv4
+	s.bind((host,port))	#bind() takes only one argument
 
 	s.listen(1)
 	s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-	
+	#socket.setsockopt(level, optname, value)
+	#level - The level argument specifies the protocol level at which the option resides. To set options at the socket level, specify the level argument as SOL_SOCKET. 
+	#optname - SO_REUSEADDR - Specifies that the rules used in validating addresses supplied to bind() should allow reuse of local addresses, if this is supported by the protocol. This option takes an int value. This is a Boolean option.
+	#value -  The value can be an integer or a string representing a buffer.
 	s_ssl=ssl.wrap_socket(s, keyfile=keyFile, certfile=certFile, server_side=True)
+	#the above function "wraps" the socket created with the security layer of signed certificate, private key
+	#The parameter server_side is a boolean which identifies whether server-side or client-side behavior is desired from this socket, true specifies server behaviour.
 	while True:
 		try:
 			c, addr = s_ssl.accept()
