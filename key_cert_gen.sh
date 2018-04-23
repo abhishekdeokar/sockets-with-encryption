8c2649aa6e6add9ef041bba302bd42554b7a29f3
abdeokar23 <abdeokar23@gmail.com>
2018-04-23 21:25:44 +0530

    keys and certificate generation script added

---
+++ key_cert_gen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+openssl genrsa -aes256 -out priv.pem 2048
+cat priv.pem
+openssl rsa -text -in priv.pem
+openssl rsa -in priv.pem -pubout -out pub.pem
+
+openssl req -new -key priv.pem -out cert.csr
+openssl req -text -in cert.csr -noout
+openssl x509 -req -days 365 -in cert.csr -signkey priv.pem -out cert.crt
